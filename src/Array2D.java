69ba1804224a4b5de4f5c61f6961d78d7ce3b8f6
prerana sengar <preranasengar@gmail.com>
2024-03-23 22:09:14 +0545

    array 2d

--- src/Array2D.java
+++ src/Array2D.java
@@ -3,7 +3,7 @@ public class Array2D {
         public static void main(String[] args){
 //declaration and initialization
             int arr[][]={{1,2,3},{2,4,5},{4,4,5}};
-//how to print
+    //how to print
             for(int i=0;i<3;i++){
                 for(int j=0;j<3;j++){
                     System.out.print(arr[i][j]+" ");
